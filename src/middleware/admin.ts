62e92c925d6c8b793b52310be5e00a79baf88abf
ishyverma <ishyamver@gmail.com>
2024-11-26 23:55:09 +0530

    added middleware functions

---
+++ src/middleware/admin.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken"
+
+export const adminMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(" ")[1]
+    try {
+        const verifyToken = jwt.verify(token as string, process.env.JWT_SECRET as string)
+        // @ts-ignore
+        req.adminData = verifyToken._id
+        next()
+    } catch (err) {
+        res.json({
+            message: "You are not signed in"
+        })
+    }
+}
